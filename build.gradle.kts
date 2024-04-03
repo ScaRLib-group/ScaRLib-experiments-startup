e940c13272c5ba02d60a642dfa34c377c531e4df
Davide Domini <davide.domini@unibo.it>
2024-04-03 11:43:49 +0200

    feat: dockerized

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,11 +28,20 @@ sourceSets {
     }
 }
 
+val scarlibVersion = "3.1.2"
+
 dependencies {
-    implementation("org.scala-lang:scala3-library_3:3.2.2")
-    implementation("io.github.davidedomini:scarlib-core:1.6.4")
-    implementation("io.github.davidedomini:dsl-core:1.6.4")
-    implementation("io.github.davidedomini:alchemist-scafi:1.6.4")
+       implementation("org.scala-lang:scala-library:2.13.10")
+    implementation("io.github.davidedomini:scarlib-core:$scarlibVersion")
+    implementation("io.github.davidedomini:dsl-core:$scarlibVersion")
+    implementation("io.github.davidedomini:alchemist-scafi:$scarlibVersion")
+    implementation("it.unibo.alchemist:alchemist:25.14.6")
+    implementation("it.unibo.alchemist:alchemist-incarnation-scafi:25.14.6")
+    implementation("it.unibo.alchemist:alchemist-incarnation-protelis:25.14.6")
+    implementation("it.unibo.alchemist:alchemist-swingui:25.7.1")
+    implementation("dev.scalapy:scalapy-core_2.13:0.5.3")
+    implementation("org.slf4j:slf4j-api:2.0.6")
+    implementation("ch.qos.logback:logback-classic:1.4.5")
 }
 
 
